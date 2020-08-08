2db820ac7a85bc71c96bb83ddf18f88fd253f205
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-08-08 22:20:13 +0900

    TypeGraphQLでのスキーマ定義

--- src/entity/penguin.ts
+++ src/entity/penguin.ts
@@ -1,13 +1,18 @@
 import { Entity, PrimaryColumn, Column } from "typeorm";
+import { ObjectType, Field, ID } from "type-graphql";
 
 @Entity()
+@ObjectType()
 export class Penguin {
   @PrimaryColumn({ name: "id", type: "int" })
+  @Field((type) => ID)
   public id!: number;
 
   @Column({ name: "name", type: "varchar", length: 45 })
+  @Field()
   public name!: string;
 
   @Column({ name: "genus", type: "varchar", length: 45 })
+  @Field()
   public genus!: string;
 }
